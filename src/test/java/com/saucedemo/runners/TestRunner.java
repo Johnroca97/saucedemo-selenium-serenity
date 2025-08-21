ead5e2922e566eb5014cf1eb74b9dfe68d548543
John Rodriguez <johnmrc97@outlook.com>
2025-08-21 12:36:36 -0500

    General Fixes

--- src/test/java/com/saucedemo/runners/TestRunner.java
+++ src/test/java/com/saucedemo/runners/TestRunner.java
@@ -1,22 +1,20 @@
 package com.saucedemo.runners;
 
-import io.cucumber.junit.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.runner.RunWith;
+import io.cucumber.junit.platform.engine.Cucumber;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(
-        features = "src/test/resources/features",
-        glue = "com.saucedemo.stepdefinitions",
-        tags = "@SauceDemo",
-        plugin = {
-                "pretty",                                    // Formato bonito en consola
-                "json:target/cucumber-reports/cucumber.json", // Para reportes de Serenity
-                "html:target/cucumber-html-report"           // Reporte HTML básico
-        },
-        snippets = CucumberOptions.SnippetType.CAMELCASE,   // Genera métodos en camelCase
-        dryRun = false,                                     // false = ejecuta los tests
-        monochrome = true                                   // Output más legible en consola
-)
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.saucedemo.stepdefinitions")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
+        value = "pretty, html:target/cucumber-reports/cucumber.html, json:target/cucumber-reports/cucumber.json")
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@SauceDemo")
+@ConfigurationParameter(key = SNIPPET_TYPE_PROPERTY_NAME, value = "camelcase")
 public class TestRunner {
 }
